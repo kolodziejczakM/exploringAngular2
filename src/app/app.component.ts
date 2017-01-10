7dd56afeecbb365d93bc44d42fd6cbbddbedd2d0
mk <kolodziejczak.mn@gmail.com>
2017-01-10 20:33:14 +0100

    Rest of AJAX methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,69 @@ import { ResourceService } from './resource.service';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers:[ResourceService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers:[ResourceService]
 })
 export class AppComponent {
-  title = 'Test angular 2 functionality & elements!';
-  fetchedIp:String;
-  constructor(private _resService: ResourceService) {}
+    title = 'Test angular 2 functionality & elements!';
+    fetchedIp:String;
+    constructor(private _resService: ResourceService) {}
 
-  getMyIp(){
-    let data = this._resService.getMyIp().subscribe(res=>{
-      this.fetchedIp = res.json().ip;
-      console.log("Fetched: ", this.fetchedIp);
-      return this.fetchedIp;
-    });
-  }
+    getMyIp(){
+        let data = this._resService.getMyIp().subscribe(
+            res=>{
+                this.fetchedIp = res.json().ip;
+                console.log("Fetched: ", this.fetchedIp);
+                return this.fetchedIp;
+            },
+            err=>{
+                console.log("Error on GET: ", err);
+            }
+        );
+    }
 
+    postFirstName(){
+        let sendThat = {
+            firstName: 'Marcin'
+        };
+        this._resService.postData(undefined,sendThat).subscribe(
+            res=>{
+                console.log("Response on post: ", res.json());
+            },
+            err =>{
+                console.log("Error response on post: ", err);
+            }
+        );
+    }
+    
+    changeFirstName(){
+        let updateThat = {
+            firstName: 'Lukasz'
+        };
+        this._resService.updateWhole(undefined,updateThat).subscribe(
+            res=>{
+                console.log("Response on put: ", res.json());
+            },
+            err =>{
+                console.log("Error response on put: ", err);
+            }
+        );
+    }
+
+    deleteFirstName(){
+        let deleteThat = {
+            firstName: 'Lukasz'
+        };
+        this._resService.deletePointed(undefined, deleteThat).subscribe(
+            res=>{
+                console.log("Response on delete: ", res.json());
+            },
+            err =>{
+                console.log("Error response on delete: ", err);
+            }
+        );
+    }
 
 }
--- src/app/resource.service.ts
+++ src/app/resource.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 
 
 @Injectable()
 export class ResourceService {
 
-  constructor(private http: Http) { }
+    constructor(private http: Http) { }
 
-  getMyIp() {
-     return this.http.get('http://ip.jsontest.com/');
-  }
+    getMyIp(url = 'http://ip.jsontest.com/') {
+        return this.http.get(url);
+    }
+
+    postData(url= 'https://httpbin.org/post', data:any):Observable<Response>{
+        return this.http.post(url,data);
+    }
+
+    updateWhole(url ='https://httpbin.org/put', data:any):Observable<Response>{
+        return this.http.put(url,data);
+    }
+
+    updateOnlyPart(url:string,data:any):Observable<Response>{
+        return this.http.patch(url,data);
+    }
+
+    deletePointed(url = 'https://httpbin.org/delete',data:any):Observable<Response>{
+        return this.http.delete(url,data);
+    }
 }
